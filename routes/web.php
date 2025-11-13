46e365fe173f0c749c698668797cbb6d1b4d7252
notepad1010 <jcawesomely@gmail.com>
2025-11-13 09:03:15 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+
+
+});
+
+require __DIR__.'/settings.php';
